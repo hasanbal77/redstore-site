190c5136e9884978a068cdd2cb36e8237189fcdb
hasanbal77 <hasanbal77@gmail.com>
2026-02-28 12:34:41 +0300

    Create paketler.js

---
+++ api/paketler.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+
+const paketler = [
+{
+isim: "20 GB",
+dk: "1000 DK",
+sosyal: "Sınırsız WhatsApp",
+fiyat: "299 TL"
+},
+{
+isim: "40 GB",
+dk: "2000 DK",
+sosyal: "Sınırsız Sosyal Medya",
+fiyat: "399 TL"
+},
+{
+isim: "60 GB",
+dk: "Sınırsız",
+sosyal: "Sınırsız Sosyal Medya",
+fiyat: "499 TL"
+}
+];
